d02e5cd201d6424a42c5320746bb5ba678652a42
听雨 <389625819@qq.com>
2018-11-03 16:57:10 +0800

    vaeThink

--- public/plugin/stay/view/js/supersized-init.js
+++ public/plugin/stay/view/js/supersized-init.js
@@ -3,7 +3,7 @@ jQuery(function($){
     $.supersized({
 
         // Functionality
-        slide_interval     : 2000,    // Length between transitions
+        slide_interval     : 3000,    // Length between transitions
         transition         : 1,    // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
         transition_speed   : 3000,    // Speed of transition
         performance        : 1,    // 0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
